2f4b96e57f1facafdf7d07c72856f9f7b5773420
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-07-16 20:05:58 +0900

    Create sj51.php

---
+++ phpclass1/sj51.php
@@ -0,0 +1,37 @@
+<?php
+  class NameAgeClass{
+    private $name = '선조이름';
+    public $age = 77;
+    protected $age2;
+    public function __construct(){	// 생성자
+      $this->name = '기본이름';	  
+      echo ' 생성자 이름은 ' . $this->name . ' 입니다.<br>';
+    }    	  
+    public function __destruct(){	// 소멸자
+      echo ' 소멸자 생성 ';
+    }
+    public function output(){
+      echo ' 이름은 ' . $this->name . ' 나이는 '. $this->age . '<br>';
+    }
+    public function setNameAge(string $name, int $age){
+      $this->name = $name;
+      $this->age = $age;
+    }
+    public function getName(){
+      retrun $this->name;
+    }
+    final public function globalTest(){
+      global $age;			// global 변수는 class 변수 아님
+      echo ' global $age = ' . $age . ' <br>';
+    }
+  }
+  $age = 111;
+  $nAge1 = new NameAgeClass();
+  $nAge1->age = 111;
+  $nAge1->setNameAge('sejong',33);
+  echo 'getName() = ' . $nAge1->getName(). '<br>';
+  $nAge1->output();
+  $nAge1->globalTest();
+  $nAge2 = $nAge1;
+  $nAge1->setNameAge('computer', 20);
+  $nAge2->output();
